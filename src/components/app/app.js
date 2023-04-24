d4c4ff5ad9d9ec9ea94f54f991c5099d5b768029
Damien <damienambient@gmail.com>
2023-04-24 19:37:12 +0300

    overall structure finished

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,8 @@
 import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
 import "./app.css";
 
@@ -6,7 +10,14 @@ function App() {
     return (
         <div className="app">
             <AppInfo></AppInfo>
-            <div className="search-panel"></div>
+
+            <div className="search-panel">
+                <SearchPanel></SearchPanel>
+                <AppFilter></AppFilter>
+            </div>
+
+            <EmployeesList></EmployeesList>
+            <EmployeesAddForm></EmployeesAddForm>
         </div>
     );
 }
